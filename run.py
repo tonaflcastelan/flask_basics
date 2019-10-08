01835c999935098a3314c6e42923e49d4d34087c
tonacastelan <tonatiuh.flores@konfio.mx>
2019-10-08 12:16:26 -0500

    Initial commit

---
+++ run.py
@@ -0,0 +1,114 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/index')
+@app.route('/')
+def hello_flask():
+    return 'Hello Flask!'
+
+
+@app.route('/new/')
+def query_string(greeting='hello'):
+    query_val = request.args.get('greeting', greeting)
+    return '<h1> the greeting is: {0} </h1>'.format(query_val)
+
+
+@app.route('/user')
+@app.route('/user/<name>')
+def no_query_strings(name='mina'):
+    return '<h1> hello there ! {} </h1>'.format(name)
+
+
+# strings
+@app.route('/text/<string:name>')
+def working_with_strings(name):
+    return '<h1> here is a string: ' + name + '</h1>'
+
+
+# numbers
+@app.route('/numbers/<int:num>')
+def working_with_numbers(num):
+    return '<h1> the number you picked is: ' + str(num) + '</h1>'
+
+
+# add numbers
+@app.route('/add/<int:num1>/<int:num2>')
+def adding_integers(num1, num2):
+    return '<h1> the sum is : {}'.format(num1 + num2) + '</h1>'
+
+
+# floats
+@app.route('/product/<float:num1>/<float:num2>')
+def product_two_numbers(num1, num2):
+    return '<h1> the product is : {}'.format(num1 * num2) + '</h1>'
+
+
+# rendering templates
+@app.route('/temp')
+def using_templates():
+    return render_template('hello.html')
+
+
+# jinja templates
+@app.route('/watch')
+def top_movies():
+    movie_list = ['autopsy of jane doe',
+                  'neon demon',
+                  'ghost in a shell',
+                  'kong: skull island',
+                  'john wick 2',
+                  'spiderman - homecoming']
+
+    return render_template('movies.html',
+                           movies=movie_list,
+                           name='Harry')
+
+
+# jinja templates tables
+@app.route('/tables')
+def movies_plus():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('table_data.html',
+                           movies=movies_dict,
+                           name='tona')
+
+
+# jinja - filters
+@app.route('/filters')
+def filter_data():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('filter_data.html',
+                           movies=movies_dict,
+                           name=None,
+                           film='a christmas carol')
+
+
+# jinja - macros
+@app.route('/macros')
+def jinja_macros():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('using_macros.html', movies=movies_dict)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
